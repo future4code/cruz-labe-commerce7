e2b0b0f3c76ca7e0d7b83310c9b93a39fb9f57b7
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-17 16:29:20 -0300

    Commit incial da estrutura do componente

--- src/components/Footer.js/Footer.js
+++ src/components/Footer.js/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default class Footer extends React.Component {
     render() {
-        return {
-            
-        }
+        return (
+            <footer>
+                
+            </footer>
+        )
     }
 }
