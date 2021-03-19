b03c7c6dedc12b30e126dc31301e9a37afe4a35a
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-19 15:12:14 -0300

    Definição do state e metodos básico para o main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,9 +9,51 @@ const DivPrincipal = styled.div`
 `
 
 export default class Main extends React.Component {
+    state = {
+        produtos: [
+            {
+                id: 1,
+                name: 'Camiseta Renan de Almeida',
+                value: 69.90,
+                imageUrl: '../../img/comiseta.jpg'
+            },
+
+            {
+                id: 2,
+                name: 'Camiseta Renan de Almeida',
+                value: 69.90,
+                imageUrl: '../../img/comiseta.jpg'
+            },
+
+            {
+                id: 3,
+                name: 'Camiseta Renan de Almeida',
+                value: 69.90,
+                imageUrl: '../../img/comiseta.jpg'
+            }
+        ],
+
+        produtosCarrinho: []
+    }
+
+    removerDoCarrinho = (id) => {
+        const produtosFiltrados = this.state.produtosCarrinho.filter(produto => {
+            return produto.id !== id ? true : false
+        })
+
+        this.setState({produtosCarrinho: produtosFiltrados})
+    }
+
     render() {
         return (
             <DivPrincipal>
+                {this.props.exibindoCarrinho &&
+                    <Carrinho 
+                        produtos={this.state.produtosCarrinho}
+                        remover={this.props.removerDoCarrinho}
+                    />
+                }
+                {/* {!this.props.exibindoCarrinho && //CHAMAR PRODUTOS } */}
                 <Carrinho />
             </DivPrincipal>
         )
